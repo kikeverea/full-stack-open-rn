e920c4cf2400e30e528edce2b36ab2291c2cf711
Enrique Verea <kikeverea@gmail.com>
2023-04-14 21:00:35 +0200

    Small refactor

--- rate-repository-app/src/components/RepositoryItem.jsx
+++ rate-repository-app/src/components/RepositoryItem.jsx
@@ -14,7 +14,7 @@ const layout = StyleSheet.create({
 })
 
 const styles = StyleSheet.create({
-  item: {
+  repositoryItem: {
     ...layout.stack,
     padding: 8,
     backgroundColor: 'white'
@@ -49,6 +49,17 @@ const styles = StyleSheet.create({
   }
 })
 
+const RepositoryItemHeader = ({ fullName, description, language, imageUrl }) =>
+  <View style={ layout.row }>
+    <Image
+      style={ styles.avatarImage }
+      source={{ uri: imageUrl }}/>
+    <HeaderInfo
+      fullName={ fullName }
+      description={ description }
+      language={ language }/>
+  </View>
+
 const HeaderInfo = ({ fullName, description, language }) =>
   <View style={ styles.headerInfo }>
     <Text style={ styles.infoLine }
@@ -62,39 +73,28 @@ const HeaderInfo = ({ fullName, description, language }) =>
     <Chip text={ language } />
   </View>
 
-const RepositoryItemHeader = ({ fullName, description, language, imageUrl }) =>
-  <View style={ layout.row }>
-    <Image
-      style={ styles.avatarImage }
-      source={{ uri: imageUrl }}/>
-    <HeaderInfo
-      fullName={ fullName }
-      description={ description }
-      language={ language }/>
-  </View>
-
 const Chip = ({ text }) =>
   <View style={[ layout.row, { justifyContent: 'flex-start' }, styles.infoLine ]}>
     <Text style={ [styles.chip, { flex: 0 }] } color='white'>{ text }</Text>
     <View style={{ flex: 1 }} />
   </View>
 
-const InfoItem = ({ name, value }) =>
+const RepositoryItemInfoBar = ({ repository }) =>
+  <View style={ styles.infoBar }>
+    <InfoBarItem name='Stars' value={ repository.stargazersCount }/>
+    <InfoBarItem name='Forks' value={ repository.forksCount }/>
+    <InfoBarItem name='Review' value={ repository.reviewCount }/>
+    <InfoBarItem name='Rating' value={ repository.ratingAverage }/>
+  </View>
+
+const InfoBarItem = ({ name, value }) =>
   <View style={ styles.infoBarItem }>
     <Text size='subheading' fontWeight='bold'>{ value }</Text>
     <Text size='subheading' color='textSecondary'>{ name }</Text>
   </View>
 
-const RepositoryItemInfoBar = ({ repository }) =>
-  <View style={ styles.infoBar }>
-    <InfoItem name='Stars' value={ repository.stargazersCount }/>
-    <InfoItem name='Forks' value={ repository.forksCount }/>
-    <InfoItem name='Review' value={ repository.reviewCount }/>
-    <InfoItem name='Rating' value={ repository.ratingAverage }/>
-  </View>
-
 const RepositoryItem = ({ repository }) =>
-  <View style={ styles.item }>
+  <View style={ styles.repositoryItem }>
     <RepositoryItemHeader
       fullName={ repository.fullName }
       description={ repository.description }
