cb6b2f49d0b7df7e3e99eebdbc7a4c0bff2379be
Enrique Verea <kikeverea@gmail.com>
2023-04-19 18:59:49 +0200

    Fetch repository list data from server

--- rate-repository-app/src/components/RepositoryList.jsx
+++ rate-repository-app/src/components/RepositoryList.jsx
@@ -1,4 +1,6 @@
 import { FlatList, View, StyleSheet } from 'react-native';
+import { useEffect, useState } from "react";
+
 import RepositoryItem from "./RepositoryItem";
 
 const styles = StyleSheet.create({
@@ -10,6 +12,23 @@ const styles = StyleSheet.create({
 const ItemSeparator = () => <View style={styles.separator} />;
 
 const RepositoryList = () => {
+
+  const [repositories, setRepositories] = useState()
+
+  const fetchRepositories = async () => {
+    const response = await fetch('http://192.168.43.118:5000/api/repositories')
+    const json = await response.json()
+
+    console.log('response', json)
+
+    setRepositories(json)
+  }
+
+  useEffect(() => {
+    fetchRepositories()
+  },
+  [])
+
   return (
     <FlatList
       style={{ padding: 8 }}
